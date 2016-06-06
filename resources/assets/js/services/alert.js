1d23edd735030ebd17a66f3285a6a7eab5ea048e
Jason Lewis <jason.lewis1991@gmail.com>
2016-06-06 20:33:02 +0930

    Add some docs to a couple classes for reference and what have you.

--- resources/assets/js/services/alert.js
+++ resources/assets/js/services/alert.js
@@ -1,9 +1,18 @@
 export default {
+    // Message to show within the alert contents.
     message: null,
+
+    // The type of alert, in this case it does not refer to the CSS class names, as these
+    // are associated within the alert component.
     type: null,
+
+    // Whether or not the alert is visible.
     visible: false,
+
+    // Whether or not the alert should be hidden on the next request.
     shouldHideOnNext: false,
 
+    // The main method that sets the required properties on the alert.
     alert(message, type) {
         this.message = message;
         this.type = type;
@@ -31,10 +40,14 @@ export default {
         this.alert(message, 'notice');
     },
 
+    // This expire method is called after each route is fired and will expire
+    // the alert so that on the next request it will be hidden.
     expire() {
         if (this.visible) this.shouldHideOnNext = true;
     },
 
+    // Used before a route is fired, this method will hide the alert if it
+    // has been expired by the method above.
     hideIfVisible() {
         if (this.shouldHideOnNext) this.visible = false;
     }
--- resources/assets/js/services/auth.js
+++ resources/assets/js/services/auth.js
@@ -1,8 +1,13 @@
 import User from './user.js'
 
 export default {
+    // Will contain the authenticated user when a successful attempt is made,
+    // otherwise this will default to false.
     authenticated: false,
 
+    // Attempt to authentiate by sending a POST request to the API with the users
+    // credentials. This delegates the success and failure responses to two
+    // other methods.
     attempt(context, credentials, redirect) {
         context.$http.post('auth', credentials).then(
             response => { this.success(context, response, redirect) },
@@ -10,6 +15,11 @@ export default {
         );
     },
 
+    // For successful authentication we'll hide any errors and grab the users
+    // token and data from the response. We'll then merge the token with
+    // the user and store a JSON string representation in local storage.
+    // We can then grabt he authenticated user instance and, if
+    // applicable, redirect to the intended path.
     success(context, response, redirect) {
         context.$set('errors', {});
 
@@ -24,16 +34,23 @@ export default {
         if (redirect) context.$router.go(redirect);
     },
 
+    // Failed requests will simply result in any errors on the response being
+    // set on the given context.
     failed(context, response) {
         context.$set('errors', response.data.errors);
     },
 
+    // To log the authenticated user out we set the authenticated property to
+    // false and remove the stored user data from local storage.
     logout() {
         this.authenticated = false;
 
         localStorage.removeItem('user');
     },
 
+    // This method will attempt to new up a user instance if one does not
+    // already exist. It will then return the new instance, or false, if
+    // a user is not authenticated.
     user() {
         if (this.authenticated) return this.authenticated;
 
@@ -48,6 +65,9 @@ export default {
         return false;
     },
 
+    // The check and guest methods are merely syntactical sugar for use
+    // in various places throughout the application to determine if
+    // the user is authenticated or if they are a guest.
     check() {
         return this.user();
     },
